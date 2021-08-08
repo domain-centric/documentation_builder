1ef22270e89f7701e5b5cbd6a489f01929aeca91
Nils ten Hoeve <nilstenhoeve@gmail.com>
2021-08-08 17:30:58 +0200

    Fixed #11

--- lib/builders/builder_factory.dart
+++ lib/builders/builder_factory.dart
@@ -1,17 +1,19 @@
 import 'package:build/build.dart';
 import 'package:documentation_builder/builders/documentation_builder.dart';
+import 'package:documentation_builder/builders/output_builder.dart';
 import 'package:documentation_builder/builders/markdown_template_builder.dart';
 
-/// The [build_runner] package will call its configured builder classes (see build.yaml) for each file it finds.
+/// The [DocumentationBuilder] process is executed by several builders.
+/// These are configured in the build.yaml file, which refers to this library to create these builders.
 ///
-/// The [build_runner] is started with the following command in the root of the project:
-/// $ flutter packages pub run build_runner build --delete-conflicting-outputs
-///
-/// You’d better clean up before you re-execute run builder_runner
-/// $ flutter packages pub run build_runner clean
+/// In order of execution:
+/// - [MarkdownTemplateBuilder]
+/// - TODO [TagBuilder]
+/// - TODO [LinkBuilder]
+/// - [OutputBuilder]
 
 Builder markdownTemplateBuilder(BuilderOptions builderOptions) =>
     MarkdownTemplateBuilder();
 
-Builder documentationBuilder(BuilderOptions builderOptions) =>
-    DocumentationBuilder();
+Builder outputBuilder(BuilderOptions builderOptions) =>
+    OutputBuilder();
